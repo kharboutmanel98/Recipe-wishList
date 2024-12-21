8290f1c35e0bda72d4db2f3d76b3e72929b42397
kharboutmanel98 <kharbout2017@gmail.com>
2024-12-21 14:12:21 +0100

    add paragraph and integrate card components with styles

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import styles from "./Content.module.scss";
 
 function Content() {
   return (
-    <div className='flex-fill b3'>Content</div>
-  )
+    <div className="flex-fill container p-20">
+      <h1 className="my-30">Discover our new recipes</h1>
+      <div className={`card p-20 ${styles.contentCard}`}>
+        <div className={styles.grid}>
+          <div className={styles.elem}>element</div>
+          <div className={styles.elem}>element</div>
+          <div className={styles.elem}>element</div>
+          <div className={styles.elem}>element</div>
+          <div className={styles.elem}>element</div>
+          <div className={styles.elem}>element</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Content
+export default Content;
