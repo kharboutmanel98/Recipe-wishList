96b9a526a06793f4f17e4d78f168fee3641117fc
kharboutmanel98 <kharbout2017@gmail.com>
2024-12-21 12:23:19 +0100

    add Wishlist, Login buttons, and Heart icon with corresponding styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import styles from "./Header.module.scss";
+import recipe from "../assets/images/logo.png";
 
 function Header() {
   return (
-    <div className='b1'>Header</div>
-  )
+    <header className={`${styles.header} d-flex flex-row align-items-center`}>
+      <i class="fa-solid fa-bars mr-15"></i>
+      <div className="flex-fill">
+        <img src={recipe} alt="logo-recipe" />
+      </div>
+      <ul>
+        <button className="mr-5 btn btn-reverse-primary">
+          <i class="fa-solid fa-heart mr-5"></i>
+          <span>Wishlist</span>
+        </button>
+        <button className="btn btn-primary">Login</button>
+      </ul>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
