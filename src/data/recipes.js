b61bb2151b20421f577661416a27caeb9bfc98ce
kharboutmanel98 <kharbout2017@gmail.com>
2024-12-22 19:02:40 +0100

    Add JSON recipe data from Unsplash

---
+++ src/data/recipes.js
@@ -0,0 +1,110 @@
+export const data = [
+  {
+    title: "Chocolate Donuts",
+    _id: "5f5b3f0e-4e20-45d1-a32f-678234a5f71c",
+    image:
+      "https://images.unsplash.com/photo-1612240498936-65f5101365d2?ixlib=rb-1.2.1&w=500&h=700",
+  },
+  {
+    title: "Loaded Nachos",
+    _id: "3c1f4d6a-4d4b-4b2f-94f2-5f6a3e8c1234",
+    image:
+      "https://images.unsplash.com/photo-1586511925558-a4c6376fe65f?ixlib=rb-1.2.1&w=500&h=700",
+  },
+  {
+    title: "Strawberry Yogurt",
+    _id: "6d4b5c2e-1b3f-45a4-85c7-231c7aef89b3",
+    image:
+      "https://images.unsplash.com/photo-1488477181946-6428a0291777?ixlib=rb-1.2.1&w=500&h=700",
+  },
+  {
+    title: "Pasta",
+    _id: "7e5a2d4b-9c3f-41b6-8d4a-5f8b1a2c3456",
+    image:
+      "https://images.unsplash.com/photo-1532939624-3af1308db9a5?ixlib=rb-1.2.1&w=500&h=700",
+  },
+  {
+    title: "Vegetable Salad",
+    _id: "4b1c2a3e-7d5f-4e4c-91b2-7a8d5c123f6a",
+    image:
+      "https://images.unsplash.com/photo-1568158879083-c42860933ed7?q=80&w=1372&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    title: "Grilled Chicken",
+    _id: "1a4f7b5e-8c2d-49a4-bc3f-9d6b4e1f785a",
+    image:
+      "https://images.unsplash.com/photo-1687877723436-061cc6e37fe8?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    title: "fish-with-lemons",
+    _id: "2f3b4d5a-6e7c-41f8-95b1-3c9d7a8f4e1b",
+    image:
+      "https://plus.unsplash.com/premium_photo-1674498271296-5144c596b43c?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    title: "Salmon Salad Bowl",
+    _id: "3c5f4d7a-2b8c-47e1-95a4-9f3e2d1c78b5",
+    image:
+      "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-1.2.1&w=500&h=700",
+  },
+  {
+    title: "Pancakes with Syrup",
+    _id: "4d3f5b1c-9e8a-41b2-75f6-2c9a7e4b5d8c",
+    image:
+      "https://images.unsplash.com/photo-1567620905732-2d1ec7ab7445?ixlib=rb-1.2.1&w=500&h=700",
+  },
+  {
+    title: "Pizza",
+    _id: "5a9f8c7b-1d2f-4e3c-85b4-6f7e2a4d9b1c",
+    image:
+      "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=rb-1.2.1&w=500&h=700",
+  },
+  {
+    title: "doughnut-with-toppings",
+    _id: "6b7a9f1c-5d4e-41b3-92f8-4c3a7e9d5a2f",
+    image:
+      "https://images.unsplash.com/photo-1551024601-bec78aea704b?q=80&w=1528&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    title: "Raspberry Layer Cake",
+    _id: "7c5f1b2a-9d8e-45b4-92f7-1a3e9c4d8b5a",
+    image:
+      "https://images.unsplash.com/photo-1565958011703-44f9829ba187?ixlib=rb-1.2.1&w=500&h=700",
+  },
+  {
+    title: "CheeseCake",
+    _id: "8e2d4b9f-1c5f-49b7-83a9-7f3c5d6a9e4b",
+    image:
+      "https://images.unsplash.com/photo-1560180474-e8563fd75bab?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  
+  {
+    title: "Soup",
+    _id: "49db4e5c-d458-48d2-badd-3e16ff6d3f70",
+    image:
+      "https://images.unsplash.com/photo-1476718406336-bb5a9690ee2a?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    title: "egg-sandwich",
+    _id: "f34a9d5e-6d73-4b59-ae81-9c53d3f77a01",
+    image:
+      "https://images.unsplash.com/photo-1550935770-d58cbf30c003?q=80&w=1450&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+  },{
+    title: "riz-chicken",
+    _id: "a23df4b8-89fc-44c6-8ab9-ea6c4f3d1b9",
+    image:
+      "https://images.unsplash.com/photo-1589302168068-964664d93dc0?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    title: "Ice Cream",
+    _id: "7bcf4de8-2e7c-43a5-bf34-d0ad5b9a612c",
+    image:
+      "https://images.unsplash.com/photo-1497034825429-c343d7c6a68f?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    title: "Crepe",
+    _id: "e18a4d6b-45fc-423d-9d7e-4fbce3a8c9c7",
+    image:
+      "https://images.unsplash.com/photo-1519159904364-0edd185dd509?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+  }
+];
