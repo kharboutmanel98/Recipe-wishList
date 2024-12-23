e41de8dc25a574753d811f9521e9798b3470c9d2
kharboutmanel98 <kharbout2017@gmail.com>
2024-12-23 23:40:11 +0100

    add search bar with icon and styles in Content component

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,17 +1,35 @@
 import styles from "./Content.module.scss";
 import Recipe from "./Recipe";
 import { data } from "../data/recipes";
+import { useState } from "react";
 
 function Content() {
   const recipes = data;
+  const [filter, setFilter] = useState("");
+  function handleInput(e) {
+    const filter = e.target.value;
+    setFilter(filter.trim().toLowerCase());
+  }
   return (
     <div className="flex-fill container p-20">
       <h1 className="my-30">Discover our new recipes</h1>
-      <div className={`card p-20 ${styles.contentCard}`}>
+      <div className={`card p-20 d-flex flex-column ${styles.contentCard}`}>
+        <div
+          className={`d-flex flex-row justify-content-center align-item-center my-30 ${styles.searchBar}`}
+        >
+          <i className="fa-solid fa-magnifying-glass mr-15"></i>
+          <input
+            type="text"
+            placeholder="Search"
+            className="flex-fill" onInput={handleInput}
+          />
+        </div>
         <div className={styles.grid}>
-          {recipes.map((r) => (
-            <Recipe title={r.title} image={r.image} />
-          ))}
+          {recipes
+            .filter((r) => r.title.toLowerCase().startsWith(filter))
+            .map((r) => (
+              <Recipe key={r._id} title={r.title} image={r.image} />
+            ))}
         </div>
       </div>
     </div>
